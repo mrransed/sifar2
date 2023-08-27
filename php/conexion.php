38f64ee11bbe260ea9078d23b3c35180d060f8ad
jorge andres moreno alvarez <ransedweb@gmail.com>
2023-08-27 05:30:03 -0500

    se crea el archivo conexion y el procesar login pero procesar login todavia no funciona

---
+++ php/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db="sifar";
+
+    $con=mysqli_connect($host,$user,$pass,$db)or die
+    ("error en la consulta".mysqli_error($con));
+
+
+
+
+?>
